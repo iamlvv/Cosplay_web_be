fe29bf0bf8821b65755be009a70c5d381daf979b
iamlvv <levanvy28062002@gmail.com>
2023-12-03 21:59:10 +0700

    fix zalopay config

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,15 @@
 export const PER_PAGE = 2;
 
+// export const ZALOPAY_CONFIG = {
+//   app_id: '2553',
+//   key1: 'PcY4iZIKFCIdgZvA6ueMcMHHUbRLYjPL',
+//   key2: 'kLtgPl8HHhfvMuDHPwKfgfsY4Ydm9eIz',
+//   endpoint: 'https://sb-openapi.zalopay.vn/v2/create',
+// };
+
 export const ZALOPAY_CONFIG = {
-  app_id: '2553',
-  key1: 'PcY4iZIKFCIdgZvA6ueMcMHHUbRLYjPL',
-  key2: 'kLtgPl8HHhfvMuDHPwKfgfsY4Ydm9eIz',
+  app_id: '554',
+  key1: '8NdU5pG5R2spGHGhyO99HN1OhD8IQJBn',
+  key2: 'uUfsWgfLkRLzq6W2uNXTCxrfxs51auny',
   endpoint: 'https://sb-openapi.zalopay.vn/v2/create',
 };
